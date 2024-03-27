5d697e9f233b29bd42b997f168fd7a659d82fee2
matija <skrlama@gmail.com>
2024-03-27 15:20:17 +0100

    added login and register styles

--- src/Components/Login.tsx
+++ src/Components/Login.tsx
@@ -1,20 +1,61 @@
-import React from "react";
+import { useState } from "react";
+import Button from "./Button";
 import Input from "./Input";
 
 const Login = () => {
-  return (
-      <div className="w-full md:w-[450px]">
-        <h1 className="text-white text-center text-bold text-4xl md:text-6xl mb-10">
-          Login
-        </h1>
-        <div className="flex flex-col bg-white p-6 min-h-[150px] gap-3 w-full rounded-xl drop-shadow-xl">
-          <Input name="email" type="email" />
-          <Input name="password" type="password" />
-          <Input name="confirm-password" type="password" />
-        </div>
-      </div>
+  const [login, setLogin] = useState(true);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
 
+  const handleSingUp = () => {
+    const data = { email, password, confirmPassword };
+  };
+
+  const handleSingIn = () => {
+    const data = { email, password };
+  };
+
+  return (
+    <div className="w-full md:w-[450px]">
+      <h1 className="text-white text-center text-bold text-4xl md:text-6xl mb-10">
+        {login ? "Login" : "Register"}
+      </h1>
+      <div className="flex flex-col bg-white p-6 min-h-[150px] gap-3 w-full rounded-xl drop-shadow-xl">
+        <Input
+          name="email"
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <Input
+          name="password"
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        {!login && (
+          <Input
+            name="confirm-password"
+            type="password"
+            value={confirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}
+          />
+        )}
+        {login ? (
+          <>
+            <Button text="Login" onClick={handleSingIn}/>
+            <Button text="Register" secondary onClick={() => setLogin(false)} />
+          </>
+        ) : (
+          <>
+            <Button text="Register" onClick={handleSingUp} />
+            <Button text="Login" secondary onClick={() => setLogin(true)} />
+          </>
+        )}
+      </div>
+    </div>
   );
-}
+};
 
 export default Login;
